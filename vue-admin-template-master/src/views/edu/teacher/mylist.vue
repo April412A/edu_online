b4027ed43aabf1c34deab983ef07d4426a11996b
cyj <1540069377@qq.com>
2021-09-11 16:39:30 +0800

    前端Vue+element-ui 讲师模块后台管理

---
+++ vue-admin-template-master/src/views/edu/teacher/mylist.vue
@@ -0,0 +1,26 @@
+<script>
+import teacher from '@/api/edu/teacher'
+
+export default {
+    data() {
+        return {
+            list:null,
+            current:1,
+            limit:10,
+            total:0,
+            teacherQuery:{}
+        }
+    },
+    created() {
+        this.getTeacherList()
+    },
+    methods: {
+        getTeacherList(){
+            teacher.getTeacherListPage(this.current,this.limit,this.teacherQuery)
+            .then(response=>{
+                console.log(response)
+            })
+        }
+    },
+}
+</script>
