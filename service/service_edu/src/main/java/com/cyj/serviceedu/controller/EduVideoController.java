e4a227a0530a249e7661fca2c080b487239144df
cyj <1540069377@qq.com>
2021-10-02 11:03:57 +0800

    gittest

--- service/service_edu/src/main/java/com/cyj/serviceedu/controller/EduVideoController.java
+++ service/service_edu/src/main/java/com/cyj/serviceedu/controller/EduVideoController.java
@@ -1,9 +1,11 @@
 package com.cyj.serviceedu.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import com.cyj.commonutils.R;
+import com.cyj.serviceedu.domain.EduVideo;
+import com.cyj.serviceedu.service.EduVideoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -15,7 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/serviceedu/edu-video")
+@CrossOrigin
 public class EduVideoController {
+
+    @Autowired
+    private EduVideoService videoService;
+
+    //添加小节
+    @PostMapping
+    public R addVideo(@RequestBody EduVideo eduVideo){
+        videoService.save(eduVideo);
+        return R.ok();
+    }
 
 }
 
